a233aa0e5a911a74fd2db321a6b92512496b5534
davidhin <hello@davidhin.com>
2020-05-14 13:34:10 +0930

    produce plot and update readme

--- README.md
+++ README.md
@@ -7,22 +7,34 @@
 1. Download StackOverflow data using StackOverflow archive
 2. Lightly preprocess SO, combine title, question and answers
 3. Word matching using Wikipedia terms and expert judgement
-4. Filter posts with at least 2 unique terms
+4. Filter posts with at least 3 unique terms, or containing at least one main tag (expert judgement)
+   1. Some trap words were things like clustering, boosting, classification, nearest neighbours etc. Clustering could refer to things like Amazon clusters, and the other words were not always data science related. A heuristic of choosing at least 3 unique terms added approximately 4000 more samples which were not obtained through the tag matching method. 
 
 **Build Kaggle Dataset**
 
 1. Download Kaggle dataset 06/05/2020 at Kaggle Meta dataset
-2. Lightly preprocess
+2. Combine question with answers to that question
+
+**Combined**
+
+1. Remove samples with less than 20 words (~2000)
+2. Remove samples with more than 1000 nonstop words (~3000)
+3. Remove samples that are not in English
+4. Remove stop words using NLTK english stopwords
+5. Utilise bigrams and trigrams using Gensim
+6. Lemmatisation using Spacy
 
 ## **Topic modelling**
 
 **Selecting candidate models**
 
-1. Remove stop words using NLTK english stopwords
-2. Utilise bigrams and trigrams using Gensim
-3. Lemmatisation using Spacy
-4. Try number of topics 5...30 for both SO and Kaggle, using both Gensim and Mallet
-5. Produce coherence plot
+1. Gridsearch over mallet topic models using parameters:
+   -  Number of Topics: 5...30
+   -  Alpha: [0.01, 5, 10, 50]
+   -  Optimise Interval: [0, 20, 50]
+   -  Total models trained: 312
+   -  Used Phoenix Job Arrays to speed up training
+2. Calculated coherence score for each model and produced plot
 
 **Manually examining candidate models**
 
--- analysis/read_results.py
+++ analysis/read_results.py
@@ -1,4 +1,6 @@
-# %% Setup
+#%%#######################################################################
+#                                Load Data                               #
+##########################################################################
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,46 +9,59 @@ import seaborn as sns
 import matplotlib
 matplotlib.rcParams['font.family'] = ['Times New Roman']
 
-# Read StackOverflow
-path = '../so_topic_modelling/'
-df = pd.concat([pd.read_csv(i) for i in glob(path+'lda_plots/*.csv')])
-df = df[df["3"]==2][['0','2','3','4']].pivot('0', '4', '2').reset_index()
-df.columns = ['Topic Number', 'Gensim SO', 'Mallet SO']
-df_so = df.set_index("Topic Number").copy()
+# Get Coherence
+df = pd.concat([pd.read_csv(i) for i in glob('../hptuning/eval/*.csv')])
+df = df[['0','1','2','3']]
+df.columns=['Alpha','Number of Topics','Optimize Interval','Coherence']
 
-# Read Kaggle
-path = '../kaggle_topic_modelling/'
-df = pd.concat([pd.read_csv(i) for i in glob(path+'lda_plots/*.csv')])
-df = df[df["3"]==1][['0','2','3','4']].pivot('0', '4', '2').reset_index()
-df.columns = ['Topic Number', 'Gensim KG', 'Mallet KG']
-df_kaggle = df.set_index("Topic Number").copy()
+# Get LL/Token Scores from Models
+param = 'training MALLET LDA with'
+value = '<1000> LL/token:'
+lltokens = []
+for f in glob('../hptuning/outputs/*.err'):
+    info = [ line for line in open(f) if param in line or value in line ]
+    iparams = info[0].split()[13:19]
+    iscore = np.exp(-float(info[1].split()[2]))
+    addinfo = [iparams[1],iparams[3],iparams[5],iscore]
+    addinfo = [float(i) for i in addinfo]
+    lltokens += [addinfo]
+columns=['Number of Topics','Alpha','Optimize Interval', 'Perplexity']
+lltokendf = pd.DataFrame(lltokens, columns=columns)
 
-# Combined
-df_comb = pd.concat([df_so,df_kaggle], axis=1).reset_index()
+# Join scores
+df = df.set_index(['Alpha','Number of Topics','Optimize Interval']).join(
+        lltokendf.set_index(['Alpha','Number of Topics','Optimize Interval'])
+    ).reset_index()
 
-# Produce size by side plot
+# Clean workspace
+del addinfo,columns,f,info,iparams,iscore,lltokendf,param,value,lltokens
+
+#%%#######################################################################
+#                             Produce Plot                               #
+##########################################################################
+
+# Produce plot
 fig, ax = plt.subplots(figsize=(7,4))
 
 def lineplot(df, index, cols, ax):
     clrs = sns.color_palette("hls", 4)
     for clr, col in zip(clrs, cols):
-        ax.plot(index, col, data=df, markersize=5, linewidth=2, color=clr)
+        ax.plot(index, col, data=df, markersize=5, 
+                linewidth=2, marker='o', color=clr)
     plt.grid(True, alpha=0.3)
     plt.xticks(fontsize=15)
     plt.xticks(np.arange(5, 31, 2))
     plt.xlabel('Number of Topics', fontsize=17)
     plt.yticks(fontsize=15)
     plt.ylabel('Coherence', fontsize=17)
-    plt.legend(bbox_to_anchor=(-0.12, -0.35, 1.12, .102), 
-               loc=3, ncol=4, 
-               mode="expand", borderaxespad=0, prop={'size': 13})
 
-lineplot(df_comb, 
-         'Topic Number', 
-         ['Gensim SO', 'Mallet SO', 'Gensim KG', 'Mallet KG'], 
+lineplot(df.groupby('Number of Topics').max().reset_index(), 
+         'Number of Topics', 
+         ['Coherence'], 
          ax)
 
-plt.savefig('coherence.svg', 
+plt.savefig('../outputs/coherence.svg', 
             bbox_inches="tight", 
             dpi=300,
             format='svg')
+
